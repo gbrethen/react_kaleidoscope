b9762afd09f57a049d5c2483392ea1888f4b8e2b
Greg Brethen <gbrethen@att.net>
2019-06-12 09:44:45 -0500

    add italic text

--- src/App-Blocks/App-Blocks.js
+++ src/App-Blocks/App-Blocks.js
@@ -11,21 +11,21 @@ class AppBlocks extends React.Component {
                 <div className="card border-secondary card-button" onClick={ (x) => this.props.func('showCE')}>
                     <div className="card-body">
                         <h5 className="card-title">Common Elements</h5>
-                        <p className="card-text">This module accepts 2 arrays as input and displays the common integers in both arrays.  Click this block to try.</p>
+                        <p className="card-text">This module accepts 2 arrays as input and displays the common integers in both arrays.  <i>Click this block to try.</i></p>
                     </div>
                 </div>
                 <br />
                 <div className="card border-secondary card-button" onClick={ (x) => this.props.func('showCB')}>
                     <div className="card-body">
                         <h5 className="card-title">Checkerboard</h5>
-                        <p className="card-text">This module utilizes loops to generate a checkerboard.  Click this block to try.</p>
+                        <p className="card-text">This module utilizes loops to generate a checkerboard.  <i>Click this block to try.</i></p>
                     </div>
                 </div>
                 <br />
                 <div className="card border-secondary card-button" onClick={ (x) => this.props.func('showSR')}>
                     <div className="card-body">
                         <h5 className="card-title">String Reverse</h5>
-                        <p className="card-text">This module reverses a string without built-in functions.  Click this block to try.</p>
+                        <p className="card-text">This module reverses a string without built-in functions.  <i>Click this block to try.</i></p>
                     </div>
                 </div>
             </div>
